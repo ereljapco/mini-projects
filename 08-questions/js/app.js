71c851aaba5b4bb0d62fb5db434469d845a70026
Erel Ropeta <helloerelita@gmail.com>
2022-04-15 05:21:10 +0800

    fix: change .show-text to .question__text--open

--- 08-questions/js/app.js
+++ 08-questions/js/app.js
@@ -20,6 +20,6 @@ btns.forEach(function (btn) {
   btn.addEventListener('click', function (e) {
     const question = e.currentTarget.parentElement.parentElement;
 
-    question.classList.toggle('show-text');
+    question.classList.toggle('.question__text--open');
   });
 });
