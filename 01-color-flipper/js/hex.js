db2ac001f57d6e21bec50791ef142aba5af074ea
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 03:58:24 +0800

    feat(color-flipper): assign btn, main and color to variables

--- 01-color-flipper/js/hex.js
+++ 01-color-flipper/js/hex.js
@@ -1,2 +1,7 @@
 // create hex array with 0 - 9 and 'A' to 'F'
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+// assign btn, main, color to variables
+const btn = document.querySelector('.color-flipper__btn');
+const main = document.querySelector('.color-flipper__main');
+const color = document.querySelector('.color-flipper--color');
