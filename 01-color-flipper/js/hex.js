47e579dae5b51d2f255ee70db9c9c70c63ff7dac
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 04:05:02 +0800

    feat(color-flipper_hex): generate random number from 0 - 15 with function randomNumber

--- 01-color-flipper/js/hex.js
+++ 01-color-flipper/js/hex.js
@@ -5,3 +5,12 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const btn = document.querySelector('.color-flipper__btn');
 const main = document.querySelector('.color-flipper__main');
 const color = document.querySelector('.color-flipper--color');
+
+// generate random number within 0 to length - 1 of hex array
+function randomNumber() {
+  const number = Math.floor(Math.random() * hex.length);
+  console.log(number);
+  return number;
+}
+
+randomNumber();
