a9b8f20242cec982480669278b8b579b99431a02
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 02:46:49 +0800

    feat(color-flipper_simple): change background color of main to the value of the randomColor when btn is clicked

--- 01-color-flipper/js/simple.js
+++ 01-color-flipper/js/simple.js
@@ -1,10 +1,15 @@
 // color array
-const colorPalette = ['#fffffe', '#2d334a', '#ffd803', '#bae8e8', '#e3f6f5'];
+const colorPalette = ['#fffffe', '#2d334a', '#ffd803', '#bae8e8', '#e3f6f5', '#fbfbfb'];
 
-// select btn and assign to a variable
+// select main and btn, and assign to a variable
+const main = document.querySelector('.color-flipper__main');
 const btn = document.querySelector('#btn');
 console.log(btn);
 
 // add event listener for btn
-
+btn.addEventListener('click', function () {
+  const randomColor = 3;
+  
+  main.style.backgroundColor = colorPalette[randomColor];
+});
 // get a random color from colorPalette when btn is clicked
