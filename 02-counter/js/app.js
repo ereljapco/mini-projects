475a8b804d3a795db23814be18963b07e3a5ced4
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 05:03:13 +0800

    feat(counter_app): increment the count value if increasebutton is clicked, decrement if decrease and change value to 0 if reset

--- 02-counter/js/app.js
+++ 02-counter/js/app.js
@@ -6,3 +6,24 @@ const reset = document.querySelector('.reset');
 const increase = document.querySelector('.increase');
 const btns = document.querySelectorAll('.counter__btn');
 const count = document.querySelector('.counter__count');
+
+// initialize counter variable with 0 as value
+let counter = 0;
+
+// add event listener to each btn
+btns.forEach(function(btn) {
+  btn.addEventListener('click', function() {
+    // check if btn has increase class
+    if (btn.classList.contains('increase')) {
+      counter++;
+    // check if btn has decrease class and is greater than 0
+    } else if (btn.classList.contains('decrease') && counter > 0) {
+      counter--;
+    } else {
+      counter = 0;
+    }
+
+    // change text of count with the value of counter
+    count.textContent = counter;
+  });
+});
