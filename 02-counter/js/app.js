264bae58977090b89e49e5a094e455abae40f35d
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 05:47:48 +0800

    perf(counter_app): remove decrease, reset, and increase variables

--- 02-counter/js/app.js
+++ 02-counter/js/app.js
@@ -1,9 +1,5 @@
-// assign decrease, reset, and increase buttons to variables
 // assign counter__btn to btns variable
 // assign counter__count to a variable
-const decrease = document.querySelector('.decrease');
-const reset = document.querySelector('.reset');
-const increase = document.querySelector('.increase');
 const btns = document.querySelectorAll('.counter__btn');
 const count = document.querySelector('.counter__count');
 
