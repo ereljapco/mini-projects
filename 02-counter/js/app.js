5c32ce50e21b6671e336af88963d1ea2fba40340
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 04:43:06 +0800

    feat(counter_app): assign button elements and assign to variables

---
+++ 02-counter/js/app.js
@@ -0,0 +1,8 @@
+// assign decrease, reset, and increase buttons to variables
+const decrease = document.querySelector('.decrease');
+const reset = document.querySelector('.reset');
+const increase = document.querySelector('.increase');
+
+console.log(decrease);
+console.log(reset);
+console.log(increase);
