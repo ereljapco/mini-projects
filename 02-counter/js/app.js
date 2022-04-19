cb4c13ef9fc7375ede3ba6438b396a49ea130159
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 04:44:52 +0800

    feat(counter_app): assign counter__count to count variable

--- 02-counter/js/app.js
+++ 02-counter/js/app.js
@@ -1,8 +1,9 @@
 // assign decrease, reset, and increase buttons to variables
+// assign counter__count to a variable
 const decrease = document.querySelector('.decrease');
 const reset = document.querySelector('.reset');
 const increase = document.querySelector('.increase');
+const count = document.querySelector('.counter__count');
 
-console.log(decrease);
-console.log(reset);
-console.log(increase);
+console.log(count);
+
