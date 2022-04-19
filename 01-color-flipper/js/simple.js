af1669f0535e093eedc741d37b222a32a7246cbb
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 02:33:10 +0800

    feat(color-flipper_simple): assign the #btn to btn variable using querySelector

--- 01-color-flipper/js/simple.js
+++ 01-color-flipper/js/simple.js
@@ -1,1 +1,10 @@
+// color array
 const colorPalette = ['#fffffe', '#2d334a', '#ffd803', '#bae8e8', '#e3f6f5'];
+
+// select btn and assign to a variable
+const btn = document.querySelector('#btn');
+console.log(btn);
+
+// add event listener for btn
+
+// get a random color from colorPalette when btn is clicked
