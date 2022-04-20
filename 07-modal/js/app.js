3047df847ac3fa6b5738af8a0b4ddfc8cb3a4ad2
Erel Ropeta <helloerelita@gmail.com>
2022-04-21 00:42:07 +0800

    feat(modal_app): show overlay when openBtn is clicked

---
+++ 07-modal/js/app.js
@@ -0,0 +1,11 @@
+// assign open-btn, close-btn, and overlay to variables
+const openBtn = document.querySelector('.modal__open-btn');
+const closeBtn = document.querySelector('.modal__close-btn');
+const overlay = document.querySelector('.modal--overlay');
+
+// show overlay when openBtn is clicked
+openBtn.addEventListener('click', () => {
+  overlay.classList.add('modal--overlay-open');
+});
+
+// hide overlay when closeBtn is clicked
