8d5514fba6766c4670b4cb12f3d06f6420109c6d
Erel Ropeta <helloerelita@gmail.com>
2022-04-21 01:52:08 +0800

    minor(questions): remove text variable in app.js

--- 08-questions/js/app.js
+++ 08-questions/js/app.js
@@ -1,7 +1,6 @@
 // assign question, toggle-btn and text to variables
 const questions = document.querySelectorAll('.question');
 const toggleBtn = document.querySelector('.question__toggle-btn');
-const text = document.querySelector('.question .question__text');
 
 // for each question, show text when toggleBtn is clicked
 questions.forEach((question) => {
