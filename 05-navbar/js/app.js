6b115f52225a7751300d588a828222b3b690411b
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 23:47:41 +0800

    feat(navbar_app): show or hide links when toggle-btn is clicked

--- 05-navbar/js/app.js
+++ 05-navbar/js/app.js
@@ -1,3 +1,8 @@
 // assign toggle-btn and links to variables
 const btn = document.querySelector('.navbar__toggle-btn');
 const links = document.querySelector('.navbar__links');
+
+// add click event listener to btn
+btn.addEventListener('click', function() {
+  links.classList.toggle('navbar__links--show');
+});
