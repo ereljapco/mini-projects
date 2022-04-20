2be8c647a4b35e347a1fde4a9a787e0bc29251d0
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 14:54:24 +0800

    feat(reviews_app): assign review--btn to btns variable

--- 04-reviews/js/app.js
+++ 04-reviews/js/app.js
@@ -1,5 +1,8 @@
 // get the img, reviewer, job-title and text elements
-const img = document.querySelector('.review_img');
-const name = document.querySelector('.review_reviewer');
-const job = document.querySelector('.review_job-title');
-const text = document.querySelector('.review_text');
+const img = document.querySelector('.review__img');
+const name = document.querySelector('.review__reviewer');
+const job = document.querySelector('.review__job-title');
+const text = document.querySelector('.review__text');
+
+// get review--btn and assign to variable
+const btns = document.querySelectorAll('.review--btn');
