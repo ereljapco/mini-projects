bda927c8afcb3159320f00a6e0c8bb10f3cf20e5
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 14:50:48 +0800

    feat(reviews_app): assign img, reviewer, job-title, and text elements to variables

---
+++ 04-reviews/js/app.js
@@ -0,0 +1,5 @@
+// get the img, reviewer, job-title and text elements
+const img = document.querySelector('.review_img');
+const name = document.querySelector('.review_reviewer');
+const job = document.querySelector('.review_job-title');
+const text = document.querySelector('.review_text');
