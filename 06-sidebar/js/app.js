95d17527727b25e7ec166a567dbae3419f985a14
Erel Ropeta <helloerelita@gmail.com>
2022-04-21 00:04:11 +0800

    feat(sidebar_app): show sidebar when toggle-btn is clicked and hide it when close-btn is clicked

--- 06-sidebar/js/app.js
+++ 06-sidebar/js/app.js
@@ -2,3 +2,13 @@
 const toggleBtn = document.querySelector('.nav__toggle-btn');
 const sidebar = document.querySelector('sidebar');
 const closeBtn = document.querySelector('.sidebar__close-btn');
+
+// show sidebar when toggle-btn is clicked
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.add('sidebar--show');
+});
+
+// hide sidebar when close-btn is clicked
+closeBtn.addEventListener('click', () => {
+  sidebar.classList.remove('sidebar--show');
+});
