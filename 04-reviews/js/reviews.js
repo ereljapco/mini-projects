b5dfb076a2e9a9e41a3549a623a62e229e0d05af
Erel Ropeta <helloerelita@gmail.com>
2022-04-20 14:48:05 +0800

    feat(reviews): add reviews.js with id, name, job, and text

---
+++ 04-reviews/js/reviews.js
@@ -0,0 +1,38 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'sara jones',
+    job: 'ux designer',
+    img: 
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text: 
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias veritatis blanditiis aperiam modi id tenetur obcaecati, aliquid sapiente sed vitae"
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web developer',
+    img: 
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text: 
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Et aperiam porro mollitia aliquam similique facere debitis. Nostrum, nihil maiores? Hic!"
+  },
+  {
+    id: 3,
+    name: 'peter smith',
+    job: 'project manager',
+    img: 
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text: 
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto ratione tempore molestiae non amet enim eligendi voluptatibus cupiditate, maxime nostrum."
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'senior webd developer',
+    img: 
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text: 
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime dignissimos natus deleniti explicabo sint saepe voluptatibus minima repellendus nesciunt voluptatem."
+  }
+];
