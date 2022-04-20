1fc1cd988e29c8fc1ad1eef1c0e9954d57c49109
Erel Ropeta <helloerelita@gmail.com>
2022-04-21 00:00:26 +0800

    feat(sidebar_app): assign toggle-btn, sidebar, and close-btn to variables

---
+++ 06-sidebar/js/app.js
@@ -0,0 +1,4 @@
+// assign toggle-btn, sidebar, and close-btn to variables
+const toggleBtn = document.querySelector('.nav__toggle-btn');
+const sidebar = document.querySelector('sidebar');
+const closeBtn = document.querySelector('.sidebar__close-btn');
