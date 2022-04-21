c986fb5bd99162fa6c5774992f46523ac0d36187
Erel Ropeta <helloerelita@gmail.com>
2022-04-22 03:35:35 +0800

    feat(menu): add menu array to menu.js

---
+++ 09-menu/js/menu.js
@@ -0,0 +1,74 @@
+const menu = [
+  {
+    id: 1,
+    category: 'Pasta',
+    img: '../images/pesto.jpg',
+    title: 'Pesto',
+    price: '309.00',
+    description: 
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore laboriosam excepturi! Quo, officia.'
+  },
+  {
+    id: 2,
+    category: 'Pasta',
+    img: '../images/carbonara.jpg',
+    title: 'Carbonara',
+    price: '255.00',
+    description: 
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore laboriosam excepturi! Quo, officia.'
+  },
+  {
+    id: 3,
+    category: 'Pasta',
+    img: '../images/baked-mac.jpg',
+    title: 'Baked Mac',
+    price: '269.00',
+    description: 
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore laboriosam excepturi! Quo, officia.'
+  },
+  {
+    id: 4,
+    category: 'Pizza',
+    img: '../images/cheese-pizza.jpg',
+    title: 'Four Cheese Pizza',
+    price: '311.00',
+    description: 
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore laboriosam excepturi! Quo, officia.'
+  },
+  {
+    id: 5,
+    category: 'Pizza',
+    img: '../images/mushroom-cheese.jpg',
+    title: 'Mushroom and Cheese Pizza',
+    price: '321.00',
+    description: 
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore laboriosam excepturi! Quo, officia.'
+  },
+  {
+    id: 6,
+    category: 'Pizza',
+    img: '../images/pepperoni-pizza.jpg',
+    title: 'Pesto',
+    price: '341.00',
+    description: 
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore laboriosam excepturi! Quo, officia.'
+  },
+  {
+    id: 7,
+    category: 'Salad',
+    img: '../images/avocado-salad.jpg',
+    title: 'Avocado Salad',
+    price: '219.00',
+    description: 
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore laboriosam excepturi! Quo, officia.'
+  },
+  {
+    id: 8,
+    category: 'Salad',
+    img: '../images/tuna-salad.jpg',
+    title: 'Tuna Salad',
+    price: '219.00',
+    description: 
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore laboriosam excepturi! Quo, officia.'
+  }
+];
