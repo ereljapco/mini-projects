b4b0f8830c35b2629ab0187f8016eb83889f1fa2
Erel Ropeta <helloerelita@gmail.com>
2022-04-22 01:09:08 +0800

    feat(questions): show/hide .question__text of the current question when its toggle-btn is clicked

--- 08-questions/js/app.js
+++ 08-questions/js/app.js
@@ -1,10 +1,17 @@
 // assign question, toggle-btn and text to variables
 const questions = document.querySelectorAll('.question');
-const toggleBtn = document.querySelector('.question__toggle-btn');
 
 // for each question, show text when toggleBtn is clicked
 questions.forEach((question) => {
+  // select toggle-btn of question
+  const toggleBtn = question.querySelector('.question__toggle-btn');
+
   toggleBtn.addEventListener('click', () => {
+    // remove .question__text--open on all question
+    questions.forEach((q) => {
+      q.classList.remove('question__text--open');
+    });
+    // add .question __text--open on the selected question
     question.classList.toggle('question__text--open');
   });
 });
