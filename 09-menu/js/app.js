6fdfab91068983887de3d2dd11d555294f11f7f0
Erel Ropeta <helloerelita@gmail.com>
2022-04-22 04:01:45 +0800

    feat(menu): display the items from menu array inside menu__items in index.html

---
+++ 09-menu/js/app.js
@@ -0,0 +1,33 @@
+// select menu__items
+const menuContainer = document.querySelector('.menu__items');
+
+// add event listener of DOMContentLoaded to window
+window.addEventListener('DOMContentLoaded', function() {
+  // create a displayMenu array using map method
+  let displayMenu = menu.map(function(item) {
+    return `<!-- menu item -->
+            <article class="menu__item">
+              <!-- menu item img -->
+              <img
+                class="menu__item-img img"
+                src="${item.img}"
+                alt="${item.title}"
+              />
+              <!-- menu item info -->
+              <div class="menu__item-info">
+                <div class="menu__item-header">
+                  <h2 class="menu__item-title">${item.title}</h2>
+                  <p class="menu__item-price">&#8369; ${item.price}</p>
+                </div>
+                <p class="menu__item-text">
+                  ${item.description}
+                </p>
+              </div>
+            </article>
+            <!-- end of menu item -->`;
+  });
+  // remove comma between items using join('')
+  displayMenu = displayMenu.join('');
+
+  menuContainer.innerHTML = displayMenu;
+});
