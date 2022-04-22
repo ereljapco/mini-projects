aa0570b2a00f7df4cd6e3faf92e84f6fd1f5eec6
Erel Ropeta <helloerelita@gmail.com>
2022-04-22 09:32:16 +0800

    feat(menu): display menu items based on the category selected

--- 09-menu/js/app.js
+++ 09-menu/js/app.js
@@ -1,27 +1,13 @@
-// select menu__items and menu__filter
+// select menu__items, menu__filter
 const menuContainer = document.querySelector('.menu__items');
 const menuFilterContainer = document.querySelector('.menu__filter');
 
 // add event listener of DOMContentLoaded to window
 window.addEventListener('DOMContentLoaded', function() {
   
-  // create a category array
-  let category = menu.reduce(function(menuCategories, cat) {
-    if (!menuCategories.includes(cat.category)) {
-      menuCategories.push(cat.category);
-    }
-    return menuCategories;
-  }, ['all']);
-
-  let displayCategories = category.map(function(category) {
-    return `<button class="menu__filter-btn btn" data-category="${category}">${category}</button>`;
-  });
-
-  displayCategories = displayCategories.join('');
-  
-  menuFilterContainer.innerHTML = displayCategories;
-  
   displayMenuItems(menu);
+  displayFilteredMenuItems(menu);
+  
 });
 
 function displayMenuItems(menuItems) {
@@ -52,4 +38,46 @@ function displayMenuItems(menuItems) {
   displayMenu = displayMenu.join('');
 
   menuContainer.innerHTML = displayMenu;
+}
+
+function displayFilteredMenuItems(menuItems) {
+  // create a category array
+  let category = menuItems.reduce(function(menuCategories, cat) {
+    if (!menuCategories.includes(cat.category)) {
+      menuCategories.push(cat.category);
+    }
+    return menuCategories;
+  }, ['All']);
+  
+  let displayCategories = category.map(function(category) {
+    return `<button class="menu__filter-btn btn" data-category="${category}">${category}</button>`;
+  });
+  
+  displayCategories = displayCategories.join('');
+  
+  menuFilterContainer.innerHTML = displayCategories;
+  
+  // display menu items based on category selected
+  const filterBtns = document.querySelectorAll('.menu__filter-btn');
+  
+  filterBtns.forEach(function(btn) {
+    let filteredMenu = menuItems.reduce(function(filtered, item) {
+      
+      if (btn.dataset.category === item.category) {
+        filtered.push(item);
+      }
+      
+      return filtered;
+    }, []);
+    
+    let currentMenuItems = menuItems;
+    
+    if (btn.dataset.category !== 'All') {
+      currentMenuItems = filteredMenu;
+    }
+    
+    btn.addEventListener('click', function() {
+      displayMenuItems(currentMenuItems);
+    });
+  });
 }
