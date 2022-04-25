90b281f7a0872a2a476939ea1ec5c32220c510dd
Erel Ropeta <helloerelita@gmail.com>
2022-04-25 11:50:04 +0800

    style(scroll): change navbar position to fixed when user scroll down

--- 12-scroll/js/app.js
+++ 12-scroll/js/app.js
@@ -1,8 +1,9 @@
-// select #date, .navbar__toggle-btn, .navbar__list--container, .navbar__list
+// select #date, .navbar__toggle-btn, .navbar__list--container, .navbar__list, .navbar
 const date = document.querySelector('#date');
 const navToggleBtn = document.querySelector('.navbar__toggle-btn');
 const navbarListContainer = document.querySelector('.navbar__list--container');
 const navbarList = document.querySelector('.navbar__list');
+const navbar = document.querySelector('.navbar');
 
 // add current year to date
 date.textContent = new Date().getFullYear();
@@ -16,5 +17,18 @@ navToggleBtn.addEventListener('click', function() {
     navbarListContainer.style.height = `${navListHeight}px`;
   } else {
     navbarListContainer.style.height = 0;
+  }
+});
+
+// add .navbar--fixed to .navbar when user scroll down
+window.addEventListener('scroll', function() {
+  const navbarHeight = navbar.getBoundingClientRect().height;
+  
+  console.log(navbarHeight);
+  
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--fixed');
+  } else {
+    navbar.classList.remove('navbar--fixed');
   }
 });
