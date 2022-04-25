c6c74d4e2fc1293c3be74f498591b1b547a094ff
Erel Ropeta <helloerelita@gmail.com>
2022-04-25 12:08:45 +0800

    feat(scroll): add back to top link when user scroll past the header

--- 12-scroll/js/app.js
+++ 12-scroll/js/app.js
@@ -1,9 +1,10 @@
-// select #date, .navbar__toggle-btn, .navbar__list--container, .navbar__list, .navbar
+// select #date, .navbar__toggle-btn, .navbar__list--container, .navbar__list, .navbar, .back-to-top
 const date = document.querySelector('#date');
 const navToggleBtn = document.querySelector('.navbar__toggle-btn');
 const navbarListContainer = document.querySelector('.navbar__list--container');
 const navbarList = document.querySelector('.navbar__list');
 const navbar = document.querySelector('.navbar');
+const backToTop = document.querySelector('.back-to-top');
 
 // add current year to date
 date.textContent = new Date().getFullYear();
@@ -23,12 +24,17 @@ navToggleBtn.addEventListener('click', function() {
 // add .navbar--fixed to .navbar when user scroll down
 window.addEventListener('scroll', function() {
   const navbarHeight = navbar.getBoundingClientRect().height;
-  
-  console.log(navbarHeight);
+  const headerHeight = document.querySelector('#home').getBoundingClientRect().height;
   
   if (window.scrollY > navbarHeight) {
     navbar.classList.add('navbar--fixed');
   } else {
     navbar.classList.remove('navbar--fixed');
+  }
+
+  if (window.scrollY > headerHeight - (headerHeight * .5)) {
+    backToTop.classList.add('back-to-top--show');
+  } else {
+    backToTop.classList.remove('back-to-top--show');
   }
 });
