2caa7088e4b00ede04a2fdbf75c6fcfe07758514
Erel Ropeta <helloerelita@gmail.com>
2022-04-25 11:02:30 +0800

    feat(scroll): show navbar__list when user click navbar__toggle-btn in mobile view

--- 12-scroll/js/app.js
+++ 12-scroll/js/app.js
@@ -1,5 +1,20 @@
-// select #date
+// select #date, .navbar__toggle-btn, .navbar__list--container, .navbar__list
 const date = document.querySelector('#date');
+const navToggleBtn = document.querySelector('.navbar__toggle-btn');
+const navbarListContainer = document.querySelector('.navbar__list--container');
+const navbarList = document.querySelector('.navbar__list');
 
 // add current year to date
 date.textContent = new Date().getFullYear();
+
+// show navbarList when user click toggleBtn by adding .navbar__list--show
+navToggleBtn.addEventListener('click', function() {
+  const navListContainerHeight = navbarListContainer.getBoundingClientRect().height;
+  const navListHeight = navbarList.getBoundingClientRect().height;
+  
+  if (navListContainerHeight === 0) {
+    navbarListContainer.style.height = `${navListHeight}px`;
+  } else {
+    navbarListContainer.style.height = 0;
+  }
+});
