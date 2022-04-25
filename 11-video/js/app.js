ab569a995fd40d5973f09f6e5cd843de8f06576f
Erel Ropeta <helloerelita@gmail.com>
2022-04-25 09:41:52 +0800

    feat(video): play or pause the video when user click switch button

--- 11-video/js/app.js
+++ 11-video/js/app.js
@@ -1,11 +1,14 @@
-// select .header__switch-btn
+// select .header__switch-btn and .header__video--container
 const switchBtn = document.querySelector('.header__switch-btn');
+const videoContainer = document.querySelector('.header__video--container');
 
 // move slider by adding .header__switch-btn--slide to switchBtn
 switchBtn.addEventListener('click', function() {
   if (!switchBtn.classList.contains('header__switch-btn--slide')) {
     switchBtn.classList.add('header__switch-btn--slide');
+    videoContainer.pause();
   } else {
     switchBtn.classList.remove('header__switch-btn--slide');
+    videoContainer.play();
   }
 });
