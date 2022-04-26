ca446c0c80e1c222c96273c9b68c74ac6146e863
Erel Ropeta <helloerelita@gmail.com>
2022-04-26 11:49:25 +0800

    feat(tabs): show content depending on which button the user clicked

--- 13-tabs/js/app.js
+++ 13-tabs/js/app.js
@@ -3,18 +3,29 @@ const featuresBtnsContainer = document.querySelector('.features__btns');
 
 featuresBtnsContainer.addEventListener('click', function(e) {
   const featuresBtns = document.querySelectorAll('.features__btn');
+  const featuresContents = document.querySelectorAll('.features__content');
   // get the data-set value of the current btn and assign to variable 'id'
+  const id = e.target.dataset.id;
   // select .features__content with the value of the 'id'
-  
+  const featuresContent = document.querySelector(`#${id}`);
+
   // add .features__btn--active only to the current btn
   featuresBtns.forEach(function(btn) {
     if (btn !== e.target) {
       btn.classList.remove('features__btn--active');
     }
-
-    e.target.classList.add('features__btn--active');
   });
 
-  // add .features__content--active to the selected .features__content
+  e.target.classList.add('features__btn--active');
 
+  // add .features__content--active to the selected .features__content only
+  
+  featuresContents.forEach(function(content) {
+    if (content !== featuresContent) {
+      content.classList.remove('features__content--active');
+    }
+  });
+  
+  featuresContent.classList.add('features__content--active');
+  // console.log(featuresContent.classList);
 });
