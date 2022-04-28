9ac5ca193833a6559d8e6ef925a0c99609575098
Erel Ropeta <helloerelita@gmail.com>
2022-04-28 11:05:49 +0800

    feat(countdown): display days, hours, mins, and secs left before giveaway ending date

---
+++ 14-countdown/js/app.js
@@ -0,0 +1,33 @@
+// get date today and assign to variable dateToday
+const dateToday = new Date();
+const dateTodayMs = dateToday.getTime();
+
+// set variable dateEndsIn 15 days after dateToday
+const month = dateToday.getMonth();
+const date = dateToday.getDate();
+const year = dateToday.getFullYear();
+let dateEndsIn = new Date(year, month, date, 23, 59);
+dateEndsIn = dateEndsIn.setDate(dateEndsIn.getDate() + 15);
+
+// subtract dateTodayMs from dateEndsIn and assign to dateDiff
+
+const dateDiff = dateEndsIn - dateTodayMs;
+// get days, hours, mins, and secs left before dateEndsIn
+const dayMs = 24 * 60 * 60 * 1000;
+const hourMs = 60 * 60 * 1000;
+const minMs = 60 * 1000
+const daysLeft = Math.floor(dateDiff / dayMs);
+const hoursLeft = Math.floor((dateDiff % dayMs) / hourMs);
+const minsLeft = Math.floor((dateDiff % hourMs) / minMs);
+const secsLeft = Math.floor((dateDiff % minMs) / 1000);
+
+// display days, hours, mins, and secs left
+const days = document.querySelector('.giveaway__days-left');
+const hours = document.querySelector('.giveaway__hours-left');
+const mins = document.querySelector('.giveaway__mins-left');
+const secs = document.querySelector('.giveaway__secs-left');
+
+days.textContent = daysLeft;
+hours.textContent = hoursLeft;
+mins.textContent = minsLeft;
+secs.textContent = secsLeft;
