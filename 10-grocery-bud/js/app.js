3889d7cbf002ead14d76e5e5dffbfc3f61aa0689
Erel Ropeta <helloerelita@gmail.com>
2022-05-04 08:25:22 +0800

    feat(grocery-bud): add the item the user submitted on the grocery list

---
+++ 10-grocery-bud/js/app.js
@@ -0,0 +1,37 @@
+// add an item to the grocery list
+// select value of .grocery-bud__input
+// select grocery-bud__items--container
+// -- should not be visible when items is less than 0
+// select grocery-bud__items
+// create element with .grocery-bud__item
+// add ID attribute, use new Date() for the ID
+
+const newItem = document.querySelector('.grocery-bud__input');
+const groceryItemsContainer = document.querySelector(
+  '.grocery-bud__items--container'
+);
+const groceryItems = document.querySelector('.grocery-bud__items');
+const groceryForm = document.querySelector('.grocery-bud');
+
+groceryForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  if (newItem.value) {
+    const groceryItem = document.createElement('div');
+    groceryItem.classList.add('grocery-bud__item');
+    groceryItem.innerHTML = `
+    <p class="grocery-bud__item-title">${newItem.value}</p>
+    <div class="grocery-bud__item-actions">
+      <button class="grocery-bud__item-edit-btn">
+        <i class="fa-solid fa-pen-to-square"></i>
+      </button>
+      <button class="grocery-bud__item-delete-btn">
+        <i class="fa-solid fa-trash"></i>
+      </button>
+    </div>
+    `;
+    groceryItems.appendChild(groceryItem);
+    newItem.value = '';
+  } else {
+    console.log(`You didn't enter an item.`);
+  }
+});
