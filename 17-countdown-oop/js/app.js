b8bb968fcc4f77ff389d8d019d0ba38d93595bf4
Erel Ropeta <helloerelita@gmail.com>
2022-05-11 04:19:47 +0800

    refactor(counter): use ES6 class instead of using function

--- 17-countdown-oop/js/app.js
+++ 17-countdown-oop/js/app.js
@@ -1,54 +1,47 @@
-function Counter(element, value) {
-  this.value = value;
-  this.valueDOM = element.querySelector('.value');
-  this.valueDOM.textContent = this.value;
-  this.decreaseBtn = element.querySelector('.decrease-btn');
-  this.resetBtn = element.querySelector('.reset-btn');
-  this.increaseBtn = element.querySelector('.increase-btn');
+class Counter {
+  constructor(element, value) {
+    this.value = value;
+    this.valueDOM = element.querySelector('.value');
+    this.valueDOM.textContent = this.value;
+    this.decreaseBtn = element.querySelector('.decrease-btn');
+    this.resetBtn = element.querySelector('.reset-btn');
+    this.increaseBtn = element.querySelector('.increase-btn');
 
-  this.decreaseBtn.addEventListener('click', this.decrease.bind(this));
-  this.resetBtn.addEventListener('click', this.reset.bind(this));
-  this.increaseBtn.addEventListener('click', this.increase.bind(this));
+    this.decreaseBtn.addEventListener('click', this.decrease.bind(this));
+    this.resetBtn.addEventListener('click', this.reset.bind(this));
+    this.increaseBtn.addEventListener('click', this.increase.bind(this));
 
-  this.style();
-}
-
-Counter.prototype.decrease = function () {
-  this.value--;
-  this.valueDOM.textContent = this.value;
-  this.style();
-};
-
-Counter.prototype.reset = function () {
-  this.value = 0;
-  this.valueDOM.textContent = this.value;
-  this.style();
-};
-
-Counter.prototype.increase = function () {
-  this.value++;
-  this.valueDOM.textContent = this.value;
-  this.style();
-};
-
-Counter.prototype.style = function () {
-  if (this.value < 0) {
-    this.valueDOM.style.color = '#ffd803';
-  } else if (this.value > 0) {
-    this.valueDOM.style.color = '#bae8e8';
-  } else {
-    this.valueDOM.style.color = '#fffffe';
+    this.style();
   }
-};
 
-const firstCounter = new Counter(
-  checkSelect('.counter__first--container'),
-  100
-);
-const secondCounter = new Counter(
-  checkSelect('.counter__second--container'),
-  200
-);
+  decrease = function () {
+    this.value--;
+    this.valueDOM.textContent = this.value;
+    this.style();
+  };
+
+  reset = function () {
+    this.value = 0;
+    this.valueDOM.textContent = this.value;
+    this.style();
+  };
+
+  increase = function () {
+    this.value++;
+    this.valueDOM.textContent = this.value;
+    this.style();
+  };
+
+  style = function () {
+    if (this.value < 0) {
+      this.valueDOM.style.color = '#ffd803';
+    } else if (this.value > 0) {
+      this.valueDOM.style.color = '#bae8e8';
+    } else {
+      this.valueDOM.style.color = '#fffffe';
+    }
+  };
+}
 
 function checkSelect(select) {
   const element = document.querySelector(select);
@@ -61,3 +54,13 @@ function checkSelect(select) {
     );
   }
 }
+
+const firstCounter = new Counter(
+  checkSelect('.counter__first--container'),
+  100
+);
+
+const secondCounter = new Counter(
+  checkSelect('.counter__second--container'),
+  200
+);
