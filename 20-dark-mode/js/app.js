ba95f6eb4fc96b8c8dca85bf950868ed076811e5
Erel Ropeta <helloerelita@gmail.com>
2022-06-02 14:43:37 +0800

    feat: format the date of the blog posts
    - use moment.js library to format the date

--- 20-dark-mode/js/app.js
+++ 20-dark-mode/js/app.js
@@ -8,11 +8,12 @@ darkModeBtn.addEventListener('click', () => {
 const posts = blogPosts
   .map((blogPost) => {
     const { title, date, length, snippet } = blogPost;
+    const formattedDate = moment(date).format('MMMM Do YYYY');
 
     return `<article class="blog-post">
             <h2 class="blog-post__title">${title}</h2>
             <div class="blog-post__info">
-              <span class="blog-post__date">${date}</span>
+              <span class="blog-post__date">${formattedDate}</span>
               <span class="blog-post__min-read">${length} min read</span>
             </div>
             <p class="blog-post__text">
