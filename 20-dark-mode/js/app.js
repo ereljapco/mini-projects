a657b58bc4eabb310bb208c614407227003245b7
Erel Ropeta <helloerelita@gmail.com>
2022-06-02 14:35:43 +0800

    feat(blog-posts): add blog posts dynamically
    - use map method to add blog posts dynamically from `blogPosts` array

--- 20-dark-mode/js/app.js
+++ 20-dark-mode/js/app.js
@@ -1,5 +1,25 @@
 const darkModeBtn = document.querySelector('.navbar__toggle-btn');
+const blogPostsContainer = document.querySelector('.blog-posts-container');
 
 darkModeBtn.addEventListener('click', () => {
   const body = document.body.classList.toggle('dark-mode');
 });
+
+const posts = blogPosts
+  .map((blogPost) => {
+    const { title, date, length, snippet } = blogPost;
+
+    return `<article class="blog-post">
+            <h2 class="blog-post__title">${title}</h2>
+            <div class="blog-post__info">
+              <span class="blog-post__date">${date}</span>
+              <span class="blog-post__min-read">${length} min read</span>
+            </div>
+            <p class="blog-post__text">
+              ${snippet}
+            </p>
+          </article>`;
+  })
+  .join('');
+
+blogPostsContainer.innerHTML = posts;
