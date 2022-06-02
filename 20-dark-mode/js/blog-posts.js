14b6eaf034106e1756312cdd94c8f248c124c761
Erel Ropeta <helloerelita@gmail.com>
2022-06-02 14:25:04 +0800

    initial: add `blog-posts.js` to `./js`

---
+++ 20-dark-mode/js/blog-posts.js
@@ -0,0 +1,40 @@
+const blogPosts = [
+  {
+    id: 1,
+    title: 'The WET Codbase',
+    date: new Date(2020, 10, 4),
+    length: 11,
+    snippet: `I'm baby ramps kombucha gluten-free ennui swag tattooed street
+              art. Marfa biodiesel letterpress blue bottle subway tile, pop-up
+              pok pok of.`,
+  },
+  {
+    id: 2,
+    title: 'Goodby, Clean Code',
+    date: new Date(2019, 11, 22),
+    length: 5,
+    snippet: `Biodiesel artisan seitan plaid sriracha copper mug venmo shabby
+              chic. Kickstarter raclette kombucha, yr post-ironic jianbing
+              try-hard flexitarian vaporware normcore.`,
+  },
+  {
+    id: 3,
+    title: 'My Decade In Review',
+    date: new Date(2018, 8, 11),
+    length: 5,
+    snippet: `Direct trade shabby chic four dollar toast, tilde actually
+              try-hard bicycle rights aesthetic forage. Meditation keytar
+              asymmetrical tacos artisan truffaut. Pabst jean shorts roof party
+              scenester.`,
+  },
+  {
+    id: 4,
+    title: 'What Are The React Team Principles',
+    date: new Date(2015, 6, 4),
+    length: 5,
+    snippet: `Selvage street art hammock affogato VHS. Mustache shaman literally
+              wayfarers schlitz. Direct trade four loko narwhal VHS pop-up,
+              chartreuse trust fund typewriter street art swag thundercats art
+              party.`,
+  },
+];
