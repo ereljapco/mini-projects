dc9f604b77557d462db643c0b83392efb5c2bbb9
Erel Ropeta <helloerelita@gmail.com>
2022-06-10 08:43:19 +0800

    feat(products): display products dynamically

---
+++ 23-products/js/app.js
@@ -0,0 +1,43 @@
+const productsContainer = document.querySelector('.products-container');
+const productsURL = 'https://course-api.com/javascript-store-products';
+
+displayProducts();
+
+async function displayProducts() {
+  const productsList = await fetchProducts();
+
+  const products = productsList
+    .map((product) => {
+      const { id } = product;
+      const { name: title, price, image } = product.fields;
+      const imageURL = image[0].url;
+      const formatPrice = `$${price / 100}`;
+
+      return `<article class="product">
+            <a
+              class="product__link"
+              href="https://course-api.com/javascript-store-single-product?id=${id}"
+            >
+              <img
+                class="product__img"
+                src="${imageURL}"
+                alt="${title}"
+              />
+              <div class="product__info">
+                <h3 class="product__title">${title}</h3>
+                <p class="product__price">${formatPrice}</p>
+              </div>
+            </a>
+          </article>`;
+    })
+    .join('');
+
+  productsContainer.innerHTML = products;
+}
+
+async function fetchProducts() {
+  const response = await fetch(productsURL);
+  const data = await response.json();
+
+  return data;
+}
