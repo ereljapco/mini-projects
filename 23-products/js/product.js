68020b382e11b88c24b095eb826e7e89d1f46377
Erel Ropeta <helloerelita@gmail.com>
2022-06-10 14:13:59 +0800

    refactor(single-product): split the set-up of the product page into functions
    - setUpProductPage()
      - display loading while waiting for the product details
      - fetch the product details
      - changePageTitle()
        - change the document.title with the capitalized product title
      - displayProduct()
        - displayColors()
          - display the available colors for the product
        - display the product details inside the `single-product` section

--- 23-products/js/product.js
+++ 23-products/js/product.js
@@ -3,41 +3,16 @@ const params = new URLSearchParams(window.location.search);
 const id = params.get('id');
 const productURL = `https://course-api.com/javascript-store-single-product?id=${id}`;
 
-displayProduct();
+setUpProductPage();
 
-async function displayProduct() {
+async function setUpProductPage() {
   productContainer.innerHTML = `<div class="loading"></div>`;
 
   const productDetails = await fetchProduct();
-  const {
-    name: title,
-    price,
-    image,
-    description,
-    colors,
-    company,
-  } = productDetails.fields;
-  const imageURL = image[0].url;
-  const formatPrice = `$${price / 100}`;
 
-  const displayColors = colors
-    .map((color) => {
-      return `<span class="single-product__color" style="background-color: ${color}"></span>`;
-    })
-    .join('');
+  changePageTitle(productDetails.fields.name);
 
-  productContainer.innerHTML = `<img class="single-product__img" src="${imageURL}" alt="
-  ${title}" />
-        <div class="single-product__info">
-          <h2 class="single-product__title">${title}</h2>
-          <p class="single-product__company">${company}</p>
-          <p class="single-product__price">${formatPrice}</p>
-          <div class="single-product__colors">
-            ${displayColors}
-          </div>
-          <p class="single-product__text">${description}</p>
-          <button class="single-product__cart-btn">Add to Cart</button>
-        </div>`;
+  displayProduct(productDetails);
 }
 
 async function fetchProduct() {
@@ -45,4 +20,47 @@ async function fetchProduct() {
   const data = await response.json();
 
   return data;
+}
+
+function changePageTitle(title) {
+  const words = title.split(' ');
+  let capitalizedTitle = '';
+
+  words.forEach((word) => {
+    capitalizedTitle += `${word.charAt(0).toUpperCase() + word.slice(1)} `;
+  });
+
+  document.title = capitalizedTitle;
+}
+
+function displayProduct(product) {
+  const {
+    name: title,
+    price,
+    image,
+    description,
+    colors,
+    company,
+  } = product.fields;
+  const imageURL = image[0].url;
+  const formatPrice = `$${price / 100}`;
+
+  productContainer.innerHTML = `
+    <img class="single-product__img" src="${imageURL}" alt="${title}" />
+    <div class="single-product__info">
+      <h2 class="single-product__title">${title}</h2>
+      <p class="single-product__company">${company}</p>
+      <p class="single-product__price">${formatPrice}</p>
+      <div class="single-product__colors">${displayColors(colors)}</div>
+      <p class="single-product__text">${description}</p>
+      <button class="single-product__cart-btn">Add to Cart</button>
+    </div>`;
+}
+
+function displayColors(colors) {
+  return colors
+    .map((color) => {
+      return `<span class="single-product__color" style="background-color: ${color}"></span>`;
+    })
+    .join('');
 }
