b41092f9782383b880f21a45a4ad2a55f7217892
Erel Ropeta <helloerelita@gmail.com>
2022-06-16 08:35:50 +0800

    feat(cocktails): display the cocktails that matches user's search input

--- 25-cocktails/js/app.js
+++ 25-cocktails/js/app.js
@@ -1,32 +1,49 @@
+let cocktailsURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
+
 const cocktailsContainer = document.querySelector('.cocktails-container');
-const cocktailsURL =
-  'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
+const cocktailsSearchInput = document.querySelector('.cocktails__search-input');
 
 displayCocktails();
+
+cocktailsSearchInput.addEventListener('keyup', () => {
+  const cocktailsSearchValue = cocktailsSearchInput.value;
+
+  if (cocktailsSearchValue !== '') {
+    cocktailsURL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailsSearchValue}`;
+  } else {
+    cocktailsURL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
+  }
+
+  displayCocktails();
+});
 
 async function displayCocktails() {
   const cocktailsList = await fecthCocktails();
 
-  const displayCocktails = cocktailsList
-    .map(({ idDrink: id, strDrink: name, strDrinkThumb: img }) => {
-      return `<a href="./drinks.html">
-            <article class="cocktail">
-              <div class="cocktail__img-container">
-                <img
-                  class="cocktail__img"
-                  src="${img}"
-                  alt="${name}"
-                  data-id="${id}"
-                />
-                <div class="cocktail__img-overlay"></div>
-              </div>
-              <p class="cocktail__name">${name}</p>
-            </article>
-          </a>`;
-    })
-    .join('');
+  try {
+    const displayCocktails = cocktailsList
+      .map(({ idDrink: id, strDrink: name, strDrinkThumb: img }) => {
+        return `<a href="./drinks.html">
+              <article class="cocktail">
+                <div class="cocktail__img-container">
+                  <img
+                    class="cocktail__img"
+                    src="${img}"
+                    alt="${name}"
+                    data-id="${id}"
+                  />
+                  <div class="cocktail__img-overlay"></div>
+                </div>
+                <p class="cocktail__name">${name}</p>
+              </article>
+            </a>`;
+      })
+      .join('');
 
-  cocktailsContainer.innerHTML = displayCocktails;
+    cocktailsContainer.innerHTML = displayCocktails;
+  } catch (error) {
+    cocktailsContainer.innerHTML = `<p class="cocktails__error-message">Sorry, your search does not match any cocktails.</p>`;
+  }
 }
 
 async function fecthCocktails() {
