7be1d9a3999bb32a03eab07b6aae59e40348c54f
Erel Ropeta <helloerelita@gmail.com>
2022-06-17 10:12:55 +0800

    feat(app): display reviews from `data.js`

--- 26-slider/js/app.js
+++ 26-slider/js/app.js
@@ -1,1 +1,24 @@
-import people from './modules/data,js';
+import people from './modules/data.js';
+
+const reviewsContainer = document.querySelector('.reviews-container');
+
+const reviews = people
+  .map((people) => {
+    const { img, name, job, text } = people;
+
+    return `<article class="review">
+                <img
+                  class="review__img"
+                  src="${img}"
+                  alt="${name}"
+                />
+                <h2 class="review__name">${name}</h2>
+                <p class="review__job">${job}</p>
+                <p class="review__text">
+                  ${text}
+                </p>
+              </article>`;
+  })
+  .join('');
+
+reviewsContainer.innerHTML = reviews;
