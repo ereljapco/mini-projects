bebc1ffd2ad72cd434b60f366430f838e1f759ed
Erel Ropeta <helloerelita@gmail.com>
2022-06-24 12:17:39 +0800

    perf(fetch-followers): add Github authentication to avoid the rate limit

--- 28-pagination/js/modules/fetch-followers.js
+++ 28-pagination/js/modules/fetch-followers.js
@@ -1,6 +1,15 @@
+import { Octokit } from 'https://cdn.skypack.dev/@octokit/core';
+
 async function fetchFollowers(url) {
-  const response = await fetch(url);
-  const data = await response.json();
+  const octokit = new Octokit({
+    auth: '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',
+  });
+
+  const response = await octokit.request('GET /users/dmalan/followers', {
+    username: 'ereljapco',
+    per_page: 100,
+  });
+  const { data } = await response;
 
   return data;
 }
