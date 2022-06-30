dd8bc19b8d74b35b6d2e7119b1f574d613fc8094
Erel Ropeta <helloerelita@gmail.com>
2022-06-30 21:56:07 +0800

    feat(display-cart-items): increase and decrease item quantity when user clicks the `increase-btn` and `decrease-btn`

--- 30-comfy-store/js/modules/display-cart-items.js
+++ 30-comfy-store/js/modules/display-cart-items.js
@@ -29,12 +29,12 @@ function displayCartItems() {
                 <p class="item__price">${formatPrice}</p>
                 <button class="item__remove-btn">remove</button>
               </div>
-              <div class="item__qty">
-                <button class="item__add-qty">
+              <div class="item__qty-container">
+                <button class="item__increase-btn">
                   <i class="fa-solid fa-chevron-up"></i>
                 </button>
-                <p class="item__number">${amount}</p>
-                <button class="item__minus-qty">
+                <p class="item__qty">${amount}</p>
+                <button class="item__decrease-btn">
                   <i class="fa-solid fa-chevron-down"></i>
                 </button>
               </div>
@@ -45,6 +45,10 @@ function displayCartItems() {
   cartItemsContainer.innerHTML = displayCartItems;
 
   removeCartItem();
+
+  increaseCartItem();
+
+  decreaseCartItem();
 
   displayTotalAmount(cartItems);
 
@@ -94,6 +98,63 @@ function removeCartItem() {
       displayTotalQty(cartItems);
     });
   });
+}
+
+function increaseCartItem() {
+  const increaseBtns = document.querySelectorAll('.item__increase-btn');
+
+  increaseBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      updateCartItemQty(e, 'increase');
+    });
+  });
+}
+
+function decreaseCartItem() {
+  const decreaseBtns = document.querySelectorAll('.item__decrease-btn');
+
+  decreaseBtns.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+      updateCartItemQty(e);
+    });
+  });
+}
+
+function updateCartItemQty(e, action) {
+  const id = e.currentTarget.parentElement.parentElement.dataset['id'];
+  const qty = e.currentTarget.parentElement.querySelector('.item__qty');
+  let cartItems = JSON.parse(localStorage.getItem('cart'));
+
+  cartItems = cartItems.map((item) => {
+    let { id: itemID, name, price, image, colors, company, amount } = item;
+
+    if (itemID == id) {
+      if (action == 'increase') {
+        amount++;
+      } else {
+        amount--;
+      }
+
+      if (amount == 0) {
+        const currentItem = e.currentTarget.parentElement.parentElement;
+        currentItem.parentElement.removeChild(currentItem);
+      }
+
+      qty.textContent = amount;
+    }
+    return { id, name, price, image, colors, company, amount };
+  });
+
+  cartItems = cartItems.filter((item) => {
+    return item.amount !== 0;
+  });
+
+  localStorage.setItem('cart', JSON.stringify(cartItems));
+
+  cartItems = JSON.parse(localStorage.getItem('cart'));
+
+  displayTotalAmount(cartItems);
+  displayTotalQty(cartItems);
 }
 
 export default displayCartItems;
