8494e869347d6da3b6257529f0ef8fd4aab10439
Erel Ropeta <helloerelita@gmail.com>
2022-07-06 13:44:51 +0800

    clean(menu): remove logs in the console

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -3,7 +3,6 @@ import { menuOpenBtn, menu, menuOverlay } from './elements.js';
 function toggleMenu() {
   menuOpenBtn.addEventListener('click', (e) => {
     menu.classList.add('menu--display');
-    console.log(menu);
   });
 
   menuOverlay.addEventListener('click', () => {
